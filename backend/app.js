555565c3d4441d5961748cd756d8670df7633a40
Prashant Patidar <prashantpatidar1402@gmail.com>
2020-01-24 18:05:14 +0530

    DB connected

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Quiz = require('./models/quiz');
+
 const app = express();
 
+mongoose.connect('mongodb+srv://dbUser:dbUserPassword@cluster0-dcaaf.mongodb.net/quiz?retryWrites=true&w=majority')
+.then(() => {
+  console.log('connected to database')
+})
+.catch(() => {
+  console.log('connection failed');
+
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
-  console.log('First middleware');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
-});
+
 
 module.exports = app;
